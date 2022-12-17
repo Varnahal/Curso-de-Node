86efd7c0020cf93d1208659137cbea89ae6daac1
Varnahal <danielmarcelino91@gmail.com>
2022-12-17 16:16:24 -0300

    trabalhando com arquivos node

---
+++ Curso/App005.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const fs = require('fs')
+const porta = process.env.PORT
+
+const server = http.createServer((req,res)=>{
+    fs.readFile('site.html',(err,archive)=>{
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(archive)
+        return res.end()
+    })
+
+})
+
+server.listen(porta||3000,()=>{console.log('Servidor rodando')})
