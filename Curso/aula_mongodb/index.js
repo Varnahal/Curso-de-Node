c57ce70002ac2a2e5a0c3584c9042e82531e03ef
Varnahal <danielmarcelino91@gmail.com>
2022-12-27 21:18:01 -0300

    usando o regex com find para coisas mais especificas

--- Curso/aula_mongodb/index.js
+++ Curso/aula_mongodb/index.js
@@ -3,15 +3,24 @@ const url="mongodb+srv://varnahal:chuvachu@varnahal.wu5lufq.mongodb.net/?retryWr
 mongodb.connect(url,(erro,banco)=>{
     if(erro) throw erro;
     const dbo = banco.db("Varnahal");
-    const obj = {curso:"curso de react",canal:"Varnahal"}
-    // dbo.collection("Cursos").insertOne(obj,(erro,res)=>{
+    // const obj = {curso:"curso de Blender",canal:"Varnahal"}
+    //  dbo.collection("Cursos").insertOne(obj,(erro,res)=>{
+    //      if(erro)throw erro;
+    //      console.log("1 novo curso inserido")
+    //      banco.close()
+    //  })
+
+    // dbo.collection("Cursos").find({canal:"Varnahal"},{projection:{_id:0,canal:0}}).toArray((erro,res)=>{
     //     if(erro)throw erro;
-    //     console.log("1 novo curso inserido")
+    //     console.log(res[0].curso)
     //     banco.close()
     // })
-    dbo.collection("Cursos").find({canal:"Varnahal"},{projection:{_id:0,canal:0}}).toArray((erro,res)=>{
+
+    const query={curso: /c./}
+
+    dbo.collection("Cursos").find(query,{projection:{_id:0}}).toArray((erro,res)=>{
         if(erro)throw erro;
-        console.log(res[0].curso)
+        console.log(res)
         banco.close()
     })
 
