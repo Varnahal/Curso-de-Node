d403fe7200691bd002b22fb48503e2830e4d5e62
Varnahal <danielmarcelino91@gmail.com>
2022-12-27 21:07:13 -0300

    metodo find para fazer uma especie de select em mongodb

--- Curso/aula_mongodb/index.js
+++ Curso/aula_mongodb/index.js
@@ -4,9 +4,14 @@ mongodb.connect(url,(erro,banco)=>{
     if(erro) throw erro;
     const dbo = banco.db("Varnahal");
     const obj = {curso:"curso de react",canal:"Varnahal"}
-    dbo.collection("Cursos").insertOne(obj,(erro,res)=>{
+    // dbo.collection("Cursos").insertOne(obj,(erro,res)=>{
+    //     if(erro)throw erro;
+    //     console.log("1 novo curso inserido")
+    //     banco.close()
+    // })
+    dbo.collection("Cursos").find({canal:"Varnahal"},{projection:{_id:0,canal:0}}).toArray((erro,res)=>{
         if(erro)throw erro;
-        console.log("1 novo curso inserido")
+        console.log(res[0].curso)
         banco.close()
     })
 
