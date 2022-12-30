5cd4b40b368f493b757384d69e887dba4d5d9825
Varnahal <danielmarcelino91@gmail.com>
2022-12-29 22:44:24 -0300

    terminado o assunto sobre mongodb

--- Curso/aula_mongodb/index.js
+++ Curso/aula_mongodb/index.js
@@ -1,16 +1,16 @@
 const mongodb = require('mongodb').MongoClient
 const url="mongodb+srv://varnahal:chuvachu@varnahal.wu5lufq.mongodb.net/?retryWrites=true&w=majority"
-mongodb.connect(url,(erro,banco)=>{
+function pesquisanobanco(){mongodb.connect(url,async(erro,banco)=>{
     if(erro) throw erro;
     const dbo = banco.db("Varnahal");
 
-    // inserir algo no banco de dados
+    //inserir algo no banco de dados
     // const obj = [
-    //     {curso:"curso de Blender",canal:"Varnahal"},
-    //     {curso:"curso de Blender",canal:"Varnahal"},
-    //     {curso:"curso de Blender",canal:"Varnahal"}
+    //     {curso:"curso de Blender 2021",canal:"Varnahal"},
+    //     {curso:"curso de Blender 2021",canal:"Varnahal"},
+    //     {curso:"curso de Blender 2021",canal:"Varnahal"}
     // ]
-    // dbo.collection("Cursos").insertMany(obj,async (erro,res)=>{
+    // await dbo.collection("Cursos").insertMany(obj,async (erro,res)=>{
     //     if(erro)throw erro;
     //     await console.log(res.insertedCount + "novo(s) curso(s) inserido(s)")
     //     banco.close()
@@ -46,23 +46,37 @@ mongodb.connect(url,(erro,banco)=>{
     // })
 
     //Deletar algo do banco de dados(ordenação)
-    let query={curso: /.Blender/}
+    // let query={curso: /.Blender/}
 
     // dbo.collection("Cursos").delete(query,(erro,res)=>{
     //     if(erro)throw erro;
     //     console.log('um curso deletado')
     //      banco.close()
     // })
-    dbo.collection("Cursos").deleteMany(query,(erro,res)=>{
-        if(erro)throw erro;
-        console.log(res.deletedCount+ 'um curso deletado')
+    // dbo.collection("Cursos").deleteMany(query,(erro,res)=>{
+    //     if(erro)throw erro;
+    //     console.log(res.deletedCount+ 'um curso deletado')
         
+    // })
+    //atualizar dados
+
+    // let query={curso: /.Blender/}
+    // let novoObj={$set:{curso:"curso de Blender 2021"}}
+
+    // dbo.collection("Cursos").updateMany(query,novoObj,async (erro,res)=>{
+    //     if(erro)throw erro;
+    //     await console.log(res.modifiedCount+'cursos atualizados')
+    // })
+
+    //limitar resultados do find
+    //const query={curso:/.Blender./}
+     const query={}
+    const limite=3
+    dbo.collection("Cursos").find(query,{projection:{_id:0}}).limit(limite).toArray((erro,res)=>{
+        if(erro)throw erro;
+        console.log(res)
+        banco.close()
     })
 
-     dbo.collection("Cursos").find({}).toArray((erro,res)=>{
-         if(erro)throw erro;
-         console.log(res)
-         banco.close()
-     })
-
-})
+})}
+pesquisanobanco()
