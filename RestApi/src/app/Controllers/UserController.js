059837ed6df6de349cdd38dcbdec715d007af22f
Varnahal <danielmarcelino91@gmail.com>
2023-01-03 00:09:52 -0300

    finalizada o começo da integração com o mongoose('mongodb')

--- RestApi/src/app/Controllers/UserController.js
+++ RestApi/src/app/Controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require('../Models/User')
 const bcrypt = require('bcryptjs')
+const yup = require('yup')
 
 class UserControllrer{
     show(req,res){
@@ -14,10 +15,31 @@ class UserControllrer{
     async store(req,res){
 
         //validação
+        let schema = yup.object().shape({
+            name:yup.string().required(),
+            email:yup.string().email().required(),
+            password:yup.string().required(),
+        })
+        if(!(await schema.isValid(req.body))){
+            return res.status(400).json({
+                error:true,
+                message:"Dados invalidos"
+            })
+        }
+        
+        let userExists = await User.findOne({email:req.body.email})
+        if(userExists){
+            return res.status(400).json({
+                error:true,
+                message:"Usuario já cadastrado"
+            })
+        }
+
 
         const {name,email,password} = req.body
 
         const data = {name,email,password}
+
         data.password = await bcrypt.hash(data.password,8)
 
         await User.create(data,(err)=>{
--- RestApi/src/app/Models/User.js
+++ RestApi/src/app/Models/User.js
@@ -4,7 +4,7 @@ const mongoose =  require("mongoose");
 const User = mongoose.Schema(
     {
     name:{type:String,required:true},
-    name:{type:String,required:true},
+    email:{type:String,required:true},
     password:{type:String,required:true},
     },
     {
